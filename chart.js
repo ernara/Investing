6bfb922976e328952a7ab0cfe78a451321897a43
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2026-06-20 17:00:54 +0300

    Improved chart.js

--- chart.js
+++ chart.js
@@ -1,33 +1,53 @@
 let chart;
 
 function drawChart() {
-	const data = accounts.map(prefix => calculateProfit(prefix));
-	const months = Math.max(...data.map(x => x.length));
-
-	const labels = Array.from(
-	{ length: data[0].filter((_, i) => i === 0 || i % 12 === 0).length },
-	(_, i) => i
-);
-
-	const datasets = data.map((values, i) => ({
-	label: accounts[i],
-	data: values.filter((_, index) => index === 0 || index % 12 === 0)
-}));
 
 	if (chart) chart.destroy();
 
 	chart = new Chart(document.getElementById("c"), {
 		type: "line",
-		data: { labels, datasets },
+		data: {
+			labels: getAxis(),
+			datasets: getData()
+		},
 		options: {
 			responsive: true,
 			maintainAspectRatio: false,
 			interaction: {
-			mode: "index",
-			intersect: false
-	}
-}
+				mode: "index",
+				intersect: false
+			},
+			plugins: {
+				tooltip: {
+					callbacks: {
+						title: items =>
+							items[0].dataIndex === 0
+								? "Pradžia"
+								: `Metai ${items[0].dataIndex}`
+					}
+				}
+			}
+		}
 	});
+}
+
+function getData() {
+	const data = accounts.map(prefix => calculateProfit(prefix));
+
+	return data.map((values, i) => ({
+		label: accounts[i],
+		data: values.filter((_, index) => index === 0 || index % 12 === 0)
+	}));
+}
+
+function getAxis() {
+	const yearly = calculateProfit(accounts[0])
+		.filter((_, i) => i === 0 || i % 12 === 0);
+
+	return Array.from(
+		{ length: yearly.length },
+		(_, i) => i
+	);
 }
 
 document.addEventListener("input", drawChart);
