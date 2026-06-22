1497e2e5f7667be33f732197000ff652606e431e
Ernestas Araminas <ernestas.araminas@tlt-pcb.com>
2026-06-22 16:12:09 +0300

    Improve results design

--- results.js
+++ results.js
@@ -8,16 +8,22 @@ function renderResults() {
 		for (let y = 1; y < history.length / 12; y++) {
 			const year = document.createElement("div");
 			year.className = "year";
-			year.innerHTML = `Metai ${y}: ${history[y * 12].toFixed(2)} €`;
+
+			year.innerHTML = `
+				<span>Metai ${y}</span>
+				<span>${history[y * 12].toFixed(2)} €</span>
+			`;
 
 			const months = document.createElement("div");
 			months.className = "months";
 
 			for (let m = 1; m <= 12; m++) {
 				const index = (y - 1) * 12 + m;
+
 				months.innerHTML += `
 					<div class="month">
-						Mėnuo ${m}: ${history[index].toFixed(2)} €
+						<span>Mėnuo ${m}</span>
+						<span>${history[index].toFixed(2)} €</span>
 					</div>
 				`;
 			}
